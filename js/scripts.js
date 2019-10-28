a7083ac3c49f5426b8469dd665bcdc6a15c96179
Student <example@example.com>
2019-10-28 16:28:04 -0700

    adds split functionality to our project

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $("form").submit(function(event){
+
+
+    var sentence = $("input#input").val();
+    var word2 = sentence.split(' ');
+    console.log(word2[1]);
+    console.log(sentence)
+    var sentence2 = sentence.split(' ');
+    console.log(sentence2)
+    event.preventDefault()
+  });
+});
